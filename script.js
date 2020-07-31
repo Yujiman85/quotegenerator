1b11463a972cd8ee3289cdc11bb18eff2a235e01
Ty Jenkins <cientifik85@gmail.com>
2020-07-31 19:50:19 -0400

    added variables to select DOM elements

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.querySelector('#quote-container');
+const quoteText = document.querySelector('#quote');
+const authorText = document.querySelector('#author');
+const twitterBtn = document.querySelector('#twitter');
+const newQuoteBtn = document.querySelector('#new-quote');
+
 // Get Quote from API
 async function getQuote() {
     const proxyUrl = 'https://intense-caverns-51299.herokuapp.com/';
